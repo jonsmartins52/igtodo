24b2ea1551568596f17e4a11b23c7b301271c21f
Jonathan <jonathan.santosmartins@gmail.com>
2023-01-18 16:54:50 -0300

    refactor: Imagem clipboard

--- src/components/EmptyList/index.tsx
+++ src/components/EmptyList/index.tsx
@@ -1,13 +1,12 @@
 import { View, Text, Image } from "react-native";
+
 import { styles } from "./styles";
+import ClipBoard from "../../assets/Clipboard.svg";
 
 export default function EmptyList() {
   return (
     <View style={styles.container}>
-      <Image
-        style={styles.emptyIcon}
-        source={require("../../assets/Clipboard.png")}
-      />
+      <ClipBoard style={styles.emptyIcon} />
       <Text style={styles.emptyTextBold}>
         Você ainda não tem tarefas cadastradas
       </Text>
